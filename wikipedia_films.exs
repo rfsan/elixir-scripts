e31133deee622315b1a4d38b8daf039268ccc9ee
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2023-07-09 10:35:14 -0500

    Better readability

--- wikipedia_films.exs
+++ wikipedia_films.exs
@@ -4,27 +4,12 @@ Mix.install([
 ])
 
 defmodule WikiScraper do
-  def download_wiki_page(url) do
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{body: body}} ->
-        body
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect("HTTP Error: #{reason}")
-    end
-  end
-
-  def parse_html(html) do
-    {:ok, document} = Floki.parse_document(html)
-    title_element = Floki.find(document, "h1.firstHeading")
-    title = Floki.text(title_element)
-    title
-  end
-
   def scrape_wiki_page(url) do
-    html = download_wiki_page(url)
-    IO.puts(html)
-    title = parse_html(html)
-    IO.puts("Title of the page: #{title}")
+    HTTPoison.get!(url).body
+    |> Floki.parse_document!()
+    |> Floki.find("h1.firstHeading")
+    |> Floki.text()
+    |> IO.inspect(label: "Title")
   end
 end
 
